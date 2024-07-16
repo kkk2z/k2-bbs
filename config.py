544e9dd31e3cbbda0f9980b415609bde7b121a47
kkk2z <139834827+kkk2z@users.noreply.github.com>
2024-07-17 06:18:09 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
+from datetime import timedelta
 
+class Config:
+    SECRET_KEY = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///bulletin_board.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PERMANENT_SESSION_LIFETIME = timedelta(days=30)  # セッションの有効期限を1ヶ月に設定
