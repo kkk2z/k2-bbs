033840247a68a195b8e0ddd6758f32ebe7f06f85
kkk2z <139834827+kkk2z@users.noreply.github.com>
2024-07-17 06:18:34 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+db = SQLAlchemy()
+
+class Post(db.Model):
+    id = db.Column(db.String(10), primary_key=True)  # ランダムID
+    name = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Post {self.id} by {self.name}>'
