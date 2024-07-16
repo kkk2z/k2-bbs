b31a785d8c8b8b594378a23f251b6b7bc939f706
kkk2z <139834827+kkk2z@users.noreply.github.com>
2024-07-17 06:22:27 +0900

    Update models.py

--- models.py
+++ models.py
@@ -3,11 +3,19 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-class Post(db.Model):
+class User(db.Model):
     id = db.Column(db.String(10), primary_key=True)  # ランダムID
-    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(10), db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Post {self.id} by {self.name}>'
+        return f'<Post {self.id} by {self.author.username}>'
